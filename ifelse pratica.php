db5b3559ae54a31b0ce4600e6ffb74d9651e9a58
João Pedro <joaopedro453@hotmail.com>
2019-10-30 18:44:49 -0300

    ifelse prática

---
+++ ifelse pratica.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+
+    $possui_cartao = true;
+    $valor_compra = 426.89;
+    $valor_frete = 78;
+    $teve_desconto = false;
+
+    if ($possui_cartao && $valor_compra >= 400) {
+        $valor_frete = 0;
+        $teve_desconto = true;
+    } else if ($possui_cartao && $valor_compra >= 300) {
+        $valor_frete *= 0.1;
+        $teve_desconto = true;
+    } else if ($possui_cartao && $valor_compra >= 200) {
+        $valor_frete *= 0.2;
+        $teve_desconto = true;
+    } else if ($possui_cartao && $valor_compra >= 100) {
+        $valor_frete *= 0.5;
+        $teve_desconto = true;
+    }
+
+    ?>
+
+    <h1>Detalhes do pedido</h1>
+
+    <p>
+        Possuí cartão da loja?
+        <?php if ($possui_cartao) {
+            echo "SIM";
+        } else {
+            echo "NÃO";
+        } ?>
+    </p>
+
+    <p>
+        Valor da compra
+        <?= "R$ $valor_compra" ?>
+    </p>
+
+    <p>
+        Valor do frete
+        <?= "R$ $valor_frete" ?>
+    </p>
+
+    <p>
+        Teve Desconto?
+        <?php if ($teve_desconto) {
+            echo "SIM";
+        } else {
+            echo "NÃO";
+        } ?>
+
+    </p>
+
+</body>
+
+</html>
