3a68f678c1c85d5860654e41d660fe75ca3ed94e
João Pedro <joaopedro453@hotmail.com>
2019-10-30 17:16:23 -0300

    comentários

---
+++ comentarios.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+    // comentário de uma linha
+    echo "comando echo <br>";
+    # outro comentário de uma linha
+    print "comando print <hr>";
+
+    /*
+        Varias linhas
+        de comentário
+    */
+    ?>
+</body>
+
+</html>
