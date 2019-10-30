1e111f7ddfa0c8b303784980fcac90fafc914191
João Pedro <joaopedro453@hotmail.com>
2019-10-30 19:30:02 -0300

    switch

---
+++ switch.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+
+    <?php
+
+    $parametro = 15;
+
+    switch ($parametro) {
+        case 1:
+            echo 'case 1';
+            break;
+        case 2:
+            echo 'case 2';
+            break;
+        case 3:
+            echo 'case 3';
+            break;
+        default:
+            echo 'default';
+            break;
+    }
+    ?>
+
+
+</body>
+
+</html>
