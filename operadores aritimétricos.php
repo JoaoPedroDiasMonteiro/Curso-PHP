705fa437e7d16dfbf29d0137b586cf64a9767335
João Pedro <joaopedro453@hotmail.com>
2019-10-30 20:33:54 -0300

    operadores aritimétricos

---
+++ operadores aritimétricos.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $num1 = 54;
+    $num2 = 123;
+    echo "a soma entre $num1 e $num2 é igual a " . ($num1 + $num2) . '<br>';
+    echo "a subtração entre $num1 e $num2 é igual a " . ($num1 - $num2) . '<br>';
+    echo "a multiplicação entre $num1 e $num2 é igual a " . ($num1 * $num2) . '<br>';
+    echo "a divisão entre $num1 e $num2 é igual a " . ($num1 / $num2) . '<br>';
+    echo "o módulo entre $num1 e $num2 é igual a " . ($num1 % $num2) . '<br>';
+
+
+
+    ?>
+</body>
+</html>
