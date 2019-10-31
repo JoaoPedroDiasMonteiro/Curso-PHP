8fbe6c436d0a4ba16c844310659e3acffffe0961
João Pedro <joaopedro453@hotmail.com>
2019-10-30 21:54:49 -0300

    funções matemáticas

---
+++ funções matemáticas.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+
+<body>
+    <?php
+   
+    $num = 7.5;
+    echo '<br>'. ceil($num); // arredonda pra cima
+    echo '<br>'. floor($num); // arredonda pra baixo
+    echo '<br>'. round($num); // arredonda com base na fração
+    echo '<br>'. rand(99,100); // gera um valor aleatório
+    echo '<br>'. getrandmax();
+    echo '<br>'. sqrt(90)
+
+
+    ?>
+</body>
+
+</html>
