9ac10248d181a2d9ce6d7c5dfd466835182d342f
João Pedro <joaopedro453@hotmail.com>
2019-11-03 17:39:21 -0300

    Getters e Setters

--- OO/pilar abstração.php
+++ OO/pilar abstração.php
@@ -1,10 +1,35 @@
 <?php
 
 class Funcionario {
+    // atributos
     public $nome = null;
     public $telefone = null;
     public $numeroFilhos = null;
 
+    //setters
+    function setNome($nome){
+        $this->nome = $nome;
+    }
+    function setTelefone($telefone){
+        $this->telefone = $telefone;
+    }
+    function setNumeroFilhos($numeroFilhos){
+        $this->numeroFilhos = $numeroFilhos;
+    }
+
+    // getters
+    function getNome(){
+        return $this->nome;
+    }
+    function getTelefone(){
+        return $this->telefone;
+    }
+    function getNumeroFilhos(){
+        return $this->numeroFilhos;
+    }
+
+
+    // metodos
     function resumirCadFunc () {
         return "$this->nome possui $this->numeroFilhos filho(s) e seu número de telefone é $this->telefone";
     }
@@ -15,7 +40,16 @@ class Funcionario {
 }
 
 $y = new Funcionario ();
-$y->modificarNumeroFilhos(5);
-echo $y->resumirCadFunc()
+$y->setNome('José da Silva');
+$y->setNumeroFilhos('42');
+$y->setTelefone(9996666);
+echo $y->resumirCadFunc();
 
+echo '<br>';
+
+$z = new Funcionario ();
+$z->setNome('Tomas Turbando');
+$z->setNumeroFilhos('0');
+$z->setTelefone(9992222);
+echo $z->resumirCadFunc();
 ?>
